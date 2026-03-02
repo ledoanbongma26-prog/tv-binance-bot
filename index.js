4fc784c38a05422284c1996bfbbbbc3f2ba59ca9
ledoanbongma26-prog <ledoanbongma26@gmail.com>
2026-03-02 11:24:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+// test homepage
+app.get("/", (req, res) => {
+  res.send("Bot is running 🚀");
+});
+
+// nhận webhook từ TradingView
 app.post("/webhook", (req, res) => {
   console.log("🔥 Webhook received!");
   console.log("Body:", JSON.stringify(req.body, null, 2));
@@ -6,4 +17,9 @@ app.post("/webhook", (req, res) => {
     status: "received",
     data: req.body
   });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
 });
